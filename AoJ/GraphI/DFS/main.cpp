d7253816aa1f2fcbe8015239eed0aec86b2f2929
noblesseoblige6 <noblesseoblige6@gmail.com>
2016-03-11 20:55:22 +0900

    Complete DFS

--- AoJ/GraphI/DFS/main.cpp
+++ AoJ/GraphI/DFS/main.cpp
@@ -2,41 +2,36 @@
 
 using namespace std;
 
-int depth_fast_search(const vector<vector<int> >& ver, vector<int>& find_times, vector<int>& complete_times, int id, int current_time)
+const int kINF = 0;
+
+int DFS(const vector<vector<int> >& ver, vector<int>& find_times, vector<int>& complete_times, int ver_id, int current_time)
 {
-  int ver_id = id;
   stack<int> s;
-  for(int i = 0; i < ver[ver_id].size(); ++i){
-    if(find_times[ver[ver_id][i]] == 0){
-      find_times[ver[ver_id][i]] = current_time++;
-      s.push(ver[ver_id][i]);
-    }
-  }
-  if(s.empty()){
-    complete_times[ver_id] = current_time+1;
-    s.pop();
-    return complete_times[ver_id];
-  }
+  for(int i = ver[ver_id].size()-1; i >= 0; --i)
+    s.push(ver[ver_id][i]);
+
   while(!s.empty()){
-    ver_id = s.top();
-    current_time = depth_fast_search(ver, find_times, complete_times, ver_id, current_time+1);
-    // cout<<current_time<<endl;
-    complete_times[ver_id] = current_time;
+    int id = s.top();
+    if(find_times[id] == kINF){
+      find_times[id] = current_time;
+      current_time = DFS(ver, find_times, complete_times, id, current_time+1);
+      complete_times[id] = current_time++;
+    }
     s.pop();
   }
-  return complete_times[ver_id];
+  return current_time;
 }
 
-void time_stamp_DFS(const vector<vector<int> >& ver, vector<int>& find_times, vector<int>& complete_times)
+void time_stamp(const vector<vector<int> >& ver, vector<int>& find_times, vector<int>& complete_times)
 {
   stack<int> s;
   int current_time = 1;
   int n = ver.size();
   for(int i = 0; i < n; ++i){
-    if(find_times[i] == 0){
+    if(find_times[i] == kINF){
       find_times[i] = current_time;
-      current_time = depth_fast_search(ver, find_times, complete_times, i, current_time+1);
-      complete_times[i] = current_time;
+      current_time = DFS(ver, find_times, complete_times, i, current_time+1);
+      complete_times[i] = current_time++;
     }
   }
 }
@@ -58,7 +53,7 @@ int main()
   }
 
   vector<int> find_times(n), complete_times(n);
-  time_stamp_DFS(ver_edges, find_times, complete_times);
+  time_stamp(ver_edges, find_times, complete_times);
 
   for(int i = 0 ; i < n; ++i){
     cout<<i+1<<" "<<find_times[i]<<" "<<complete_times[i]<<endl;
