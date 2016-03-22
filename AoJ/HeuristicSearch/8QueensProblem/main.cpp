dcf9ef849e51cb9eea13632daaaaf3c9c289ecc9
noblesseoblige6 <noblesseoblige6@gmail.com>
2016-03-23 05:20:09 +0900

    Remove some codes for debug

--- AoJ/HeuristicSearch/8QueensProblem/main.cpp
+++ AoJ/HeuristicSearch/8QueensProblem/main.cpp
@@ -2,35 +2,38 @@
 
 using namespace std;
 
+const int nMass = 8;
+
 void check_board(vector<vector<bool> >& b, const int& r, const int& c)
 {
-  for(int i = 0; i < 8; ++i){
+  for(int i = 0; i < nMass; ++i){
     b[r][i] = b[i][c] = true;
     int tmp_r = r+i, tmp_c = c+i;
     int tmp_r2 = r-i, tmp_c2 = c-i;
 
-    if(tmp_r < 8 && tmp_c < 8)
+    if(tmp_r < nMass && tmp_c < nMass)
       b[tmp_r][tmp_c] = true;
     if(tmp_r2 >= 0 && tmp_c2 >= 0)
       b[tmp_r2][tmp_c2] = true;
 
-    if(tmp_r < 8 && tmp_c2 >= 0)
+    if(tmp_r < nMass && tmp_c2 >= 0)
       b[tmp_r][tmp_c2] = true;
-    if(tmp_r2 >= 0 && tmp_c < 8)
+    if(tmp_r2 >= 0 && tmp_c < nMass)
       b[tmp_r2][tmp_c] = true;
   }
 }
 
 bool find_q_pos(const vector<vector<bool> >& board, vector<int>& q_pos, const int& row)
 {
-  if(row == 8)
+  if(row == nMass)
     return true;
 
+  //@coomment there is already queen
   if(q_pos[row] != -1)
     if(find_q_pos(board, q_pos, row+1))
       return true;
 
-  for(int i = 0; i < 8; ++i){
+  for(int i = 0; i < nMass; ++i){
     if(!board[row][i]){
       vector<vector<bool> > tmp_board = board;
       check_board(tmp_board, row, i);
@@ -47,22 +50,21 @@ bool find_q_pos(const vector<vector<bool> >& board, vector<int>& q_pos, const in
 int main()
 {
   int n;
-  vector<vector<bool> > board(8, vector<bool>(8, false));
-  vector<int> q_pos(8, -1);
+  int r, c;
+  vector<vector<bool> > board(nMass, vector<bool>(nMass, false));
+  vector<int> q_pos(nMass, -1);
+
   cin >> n;
   for(int i = 0; i < n; ++i){
-    int r, c;
     cin >> r >> c;
     check_board(board, r, c);
     q_pos[r] = c;
   }
 
-    find_q_pos(board, q_pos, 0);
+  find_q_pos(board, q_pos, 0);
 
-  for(int i = 0; i < 8; ++i){
-    // cout<<i<<" "<<q_pos[i]<<endl;
-    for(int j = 0; j < 8; ++j){
-      // cout<<board[i][j]<<" ";
+  for(int i = 0; i < nMass; ++i){
+    for(int j = 0; j < nMass; ++j){
       if(q_pos[i] == j)
         cout<<"Q";
       else
@@ -70,5 +72,6 @@ int main()
     }
     cout<<endl;
   }
+
   return 0;
 }
