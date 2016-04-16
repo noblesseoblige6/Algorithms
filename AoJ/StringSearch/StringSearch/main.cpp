a71273b135672def7ccdcda57e3768548a1793da
a_comma <noblesseoblige6@gmail.com>
2016-04-16 18:25:21 +0900

    Developing String Search

--- AoJ/StringSearch/StringSearch/main.cpp
+++ AoJ/StringSearch/StringSearch/main.cpp
@@ -3,21 +3,32 @@ using namespace std;
 
 int main()
 {
-  string org, str;
+  string pattern, str;
 
-  cin >> org >> str;
-  if(org.size() >= str.size()){
-    int i, j;
-    for(i = 0; i < org.size()-str.size()+1; ){
-      for(j = 0; j < str.size(); ++j){
-        if(org[i+j] != str[j]){
-          i += j;
+  cin >> str >> pattern;
+  if(str.size() >= pattern.size()){
+    map<char, int> spaces;
+    int count = 1;
+    for(string::reverse_iterator i = pattern.rbegin()+1; i != pattern.rend(); ++i){
+      if(spaces.find(pattern[*i]) == spaces.end())
+        spaces[*i] = count++;
+    }
+
+    for(int i = pattern.size()-1; i < str.size(); ){
+      // cout<<i<<endl;
+      for(int j = 0; j < pattern.size(); ++j){
+        if(str[i-j] != pattern[pattern.size()-1-j]){
+          int prev = i-pattern.size()-1;
+          spaces.find(str[i-j]) == spaces.end() ? i = i-j+pattern.size() : i = i-j+spaces[str[i-j]];
+          if(i < prev)
+            i = prev + pattern.size();
+
           break;
         }
-      }
-      if(j == str.size()){
-        cout<<i<<endl;
-        ++i;
+        else if(j+1 == pattern.size()){
+          cout<<i-j<<endl;
+          i++;
+        }
       }
     }
   }
