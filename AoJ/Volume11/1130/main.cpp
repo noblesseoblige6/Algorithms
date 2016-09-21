a03339dd407a8cc26fdfa35a96b826dab1d085e0
a_comma <s1180123@gmail.com>
2016-09-22 00:31:30 +0900

    Complete Red and Black

--- AoJ/Volume11/1130/main.cpp
+++ AoJ/Volume11/1130/main.cpp
@@ -1,46 +1,48 @@
 #include<bits/stdc++.h>
+#define PrintTest cout<<"TEST"<<endl;
 using namespace std;
 const int BLACK = 0;
 const int RED = 1;
 const int VISITED = 3;
 
-int FindMovableTiles(const vector<vector<int> >& tiles, int x, int y, int w, int h)
+int FindMovableTiles(vector<vector<int> >& tiles, int sx, int sy, int w, int h)
 {
   int count = 0;
   queue<pair<int, int> > q;
+  q.push(pair<int, int>(sx, sy));
 
-  q.push(pair<int, int>(x, y));
+  const pair<int, int> offsets[4] =
+  {
+    pair<int, int>(-1, 0),
+    pair<int, int>(0, -1),
+    pair<int, int>(1, 0),
+    pair<int, int>(0, 1)
+  };
+
   while(!q.empty())
   {
     pair<int, int> coord = q.front();
+    q.pop();
+    count++;
+
     int x = coord.first;
     int y = coord.second;
-    int xl = coord.first-1;
-    int xr = coord.first+1;
-    int yu = coord.second-1;
-    int yd = coord.second+1;
 
-    if(xl >= 0)
+    for(int i = 0; i < 4; ++i)
     {
-      if(tiles[xl][y] != RED && tiles[xl][y] != VISITED)
-        q.push(pair<int, int>(xl, y));
+      int xoffset = x + offsets[i].first;
+      int yoffset = y + offsets[i].second;
+
+      if( (0 <= xoffset && xoffset < w) &&
+          (0 <= yoffset && yoffset < h) )
+      {
+        if(tiles[xoffset][yoffset] == BLACK)
+        {
+          tiles[xoffset][yoffset] = VISITED;
+          q.push(pair<int, int>(xoffset, yoffset));
+        }
+      }
     }
-    if(yu < h)
-    {
-      if(tiles[x][yu] != RED && tiles[x][yu] != VISITED)
-        q.push(pair<int, int>(x, yu));
-    }
-    if(xr < w)
-    {
-      if(tiles[xr][y] != RED && tiles[xr][y] != VISITED)
-        q.push(pair<int, int>(xr, y));
-    }
-    if(yd >= 0)
-    {
-      if(tiles[x][yd] != RED && tiles[x][yd] != VISITED)
-        q.push(pair<int, int>(x, yd));
-    }
-    count++;
   }
 
   return count;
@@ -50,29 +52,38 @@ int main()
 {
   int w, h;
   int sx, sy;
-  cin >> w >> h;
 
-  vector<vector<int> > tiles(w, vector<int>(h));
-  char sign;
-  for(int j = 0; j < h; ++j)
+  while(1)
   {
-    for(int i = 0; i < w; ++i)
+    cin >> w >> h;
+
+    if(w == 0 && h == 0)
+      break;
+
+    vector<vector<int> > tiles(w, vector<int>(h, -1));
+    char sign;
+    for(int j = 0; j < h; ++j)
     {
-      cin >> sign;
-      if(sign == '.')
+      for(int i = 0; i < w; ++i)
       {
-        tiles[i][j] = BLACK;
-      }
-      else if(sign == '#')
-      {
-        tiles[i][j] = RED;
-      }
-      else
-      {
-        tiles[i][j] = VISITED;
+        cin >> sign;
+        if(sign == '.')
+        {
+          tiles[i][j] = BLACK;
+        }
+        else if(sign == '#')
+        {
+          tiles[i][j] = RED;
+        }
+        else if(sign == '@')
+        {
+          tiles[i][j] = VISITED;
+          sx = i;
+          sy = j;
+        }
       }
     }
+    cout<<FindMovableTiles(tiles, sx, sy, w, h)<<endl;
   }
-  cout<<FindMovableTiles(tiles, sx, sy, w, h)<<endl;
   return 0;
 }
