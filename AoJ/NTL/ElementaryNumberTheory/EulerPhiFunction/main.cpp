8fe5b25cf8037ecb1facf10fe0bb0cc7736e775b
a_comma <s1180123@gmail.com>
2016-10-06 23:56:59 +0900

    temp commit

--- AoJ/NTL/ElementaryNumberTheory/EulerPhiFunction/main.cpp
+++ AoJ/NTL/ElementaryNumberTheory/EulerPhiFunction/main.cpp
@@ -3,8 +3,35 @@
 using namespace std;
 typedef unsigned long long ull;
 
+void Factorize(ull n, vector<ull>& primes)
+{
+  ull nSq = sqrt(n);
+  ull d = 2;
+  while(d <= nSq)
+  {
+    if(n % d == 0)
+    {
+      n /= d;
+      primes.push_back(d);
+      continue;
+    }
+    d++;
+  }
+  if(n != 1)
+    primes.push_back(n);
+}
+
 int  PhiFunc(int n)
 {
+  vector<ull> primes;
+  Factorize(n, primes);
+
+  double count = 1;
+  for(int i = 0; i < primes.size(); ++i)
+    count *= (1.0-1.0/primes[i]);
+  count *= n;
+
+  return (int)count;
 }
 
 int main()
