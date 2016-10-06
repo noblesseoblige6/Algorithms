38a3de32f95c244497e688145f87827fca936506
a_comma <s1180123@gmail.com>
2016-10-06 23:51:44 +0900

    Try to solve Phi function

---
+++ AoJ/NTL/ElementaryNumberTheory/EulerPhiFunction/main.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+typedef unsigned long long ull;
+
+int  PhiFunc(int n)
+{
+}
+
+int main()
+{
+  int n;
+  cin >> n;
+
+  cout <<PhiFunc(n) << endl;
+
+  return 0;
+}
