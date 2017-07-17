208c32b673b827e7d8a81e18d824f290382d70a9
a_comma <s1180123@gmail.com>
2017-07-18 00:24:31 +0900

    Add max, min, and clamp functions

--- acLib/src/Utility.h
+++ acLib/src/Utility.h
@@ -1,5 +1,4 @@
 #pragma once
-#include <limits>
 
 namespace acLib
 {
@@ -11,6 +10,21 @@ namespace acLib
             static double Lerp(const double x, const double x0, const double x1)
             {
                 return (x - x0) / (x1 - x0);
+            }
+
+            static double Max(const double& a, const double& b)
+            {
+                return a < b ? b : a;
+            }
+
+            static double Min(const double& a, const double& b)
+            {
+                return a < b ? a : b;
+            }
+
+            static double Clamp(const double& val, const double& min, const double& max)
+            {
+                return Min(Max(min, val), max);
             }
         };
     }
