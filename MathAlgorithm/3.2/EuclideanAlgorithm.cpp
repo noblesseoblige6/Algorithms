16c9d8f10560cc37ee031c253506e77d7dfd5728
a_comma <noblesseoblige6@gmail.com>
2022-01-07 23:42:17 +0900

    WIP: developing LCM

--- MathAlgorithm/3.2/EuclideanAlgorithm.cpp
+++ MathAlgorithm/3.2/EuclideanAlgorithm.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <algorithm>
+#include <iterator>
 #include <vector>
 
 namespace alg
@@ -25,6 +26,26 @@ namespace alg
 
         return res;
     }
+
+    std::uint64_t LCM(std::vector<std::uint64_t> const &values)
+    {
+        std::vector<std::uint64_t> res;
+        auto tmp = values.front();
+        for (std::int32_t i = 1; i < static_cast<std::int32_t>(values.size()); ++i)
+        {
+            auto lcm = tmp * values[i];
+            tmp = GDC(tmp, values[i]);
+            res.push_back(lcm / tmp);
+        }
+
+        tmp = res.front();
+        for (std::int32_t i = 1; i < static_cast<std::int32_t>(res.size()); ++i)
+        {
+            tmp = GDC(tmp, values[i]);
+        }
+
+        return tmp;
+    }
 }
 
 int main()
@@ -35,7 +56,10 @@ int main()
     std::vector<std::uint64_t> v(n);
     std::copy_n(std::istream_iterator<std::uint64_t>{std::cin}, v.size(), v.begin());
 
-    auto res = alg::GDC(v);
+    // 3.2.2
+    // auto res = alg::GDC(v);
+    // 3.2.3
+    auto res = alg::LCM(v);
     std::cout << res << std::endl;
 
     return 0;
