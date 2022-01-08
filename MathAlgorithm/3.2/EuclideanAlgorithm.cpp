2c74c8d6e2295d8f0d724f148d5b510b061f693d
a_comma <noblesseoblige6@gmail.com>
2022-01-08 14:31:23 +0900

    add configuration

--- MathAlgorithm/3.2/EuclideanAlgorithm.cpp
+++ MathAlgorithm/3.2/EuclideanAlgorithm.cpp
@@ -2,10 +2,11 @@
 #include <algorithm>
 #include <iterator>
 #include <vector>
+#include <numeric>
 
 namespace alg
 {
-    std::uint64_t GDC(std::uint64_t a, std::uint64_t b)
+    std::uint32_t GDC(std::uint32_t a, std::uint32_t b)
     {
         if (a == 0 || b == 0)
             return std::max(a, b);
@@ -16,7 +17,7 @@ namespace alg
         return GDC(bigger % less, less);
     }
 
-    std::uint64_t GDC(std::vector<std::uint64_t> const &values)
+    std::uint32_t GDC(std::vector<std::uint32_t> const &values)
     {
         auto res = values.front();
         for (std::int32_t i = 1; i < static_cast<std::int32_t>(values.size()); ++i)
@@ -27,38 +28,30 @@ namespace alg
         return res;
     }
 
-    std::uint64_t LCM(std::vector<std::uint64_t> const &values)
+    std::uint32_t LCM(std::vector<std::uint32_t> const& values)
     {
-        std::vector<std::uint64_t> res;
-        auto tmp = values.front();
+        auto gdc = values.front();
         for (std::int32_t i = 1; i < static_cast<std::int32_t>(values.size()); ++i)
         {
-            auto lcm = tmp * values[i];
-            tmp = GDC(tmp, values[i]);
-            res.push_back(lcm / tmp);
+            gdc = GDC(gdc, values[i]);
         }
 
-        tmp = res.front();
-        for (std::int32_t i = 1; i < static_cast<std::int32_t>(res.size()); ++i)
-        {
-            tmp = GDC(tmp, values[i]);
-        }
-
-        return tmp;
+        auto res = *(values.end()-1) * *(values.end()-2) / gdc;
+        return res;
     }
 }
 
 int main()
 {
-    std::uint64_t n;
+    std::uint32_t n;
     std::cin >> n;
 
-    std::vector<std::uint64_t> v(n);
-    std::copy_n(std::istream_iterator<std::uint64_t>{std::cin}, v.size(), v.begin());
+    std::vector<std::uint32_t> v(n);
+    std::copy_n(std::istream_iterator<std::uint32_t>{std::cin}, v.size(), v.begin());
 
     // 3.2.2
     // auto res = alg::GDC(v);
-    // 3.2.3
+    // 3.2.33
     auto res = alg::LCM(v);
     std::cout << res << std::endl;
 
