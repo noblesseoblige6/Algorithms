87f9f201c8594def67461d642278abbbbb8acd52
a_comma <noblesseoblige6@gmail.com>
2022-01-09 17:52:39 +0900

    solve LCM

--- MathAlgorithm/3.2/EuclideanAlgorithm.cpp
+++ MathAlgorithm/3.2/EuclideanAlgorithm.cpp
@@ -6,7 +6,7 @@
 
 namespace alg
 {
-    std::uint32_t GCD(std::uint32_t a, std::uint32_t b)
+    std::uint64_t GCD(std::uint64_t a, std::uint64_t b)
     {
         if (a == 0 || b == 0)
             return std::max(a, b);
@@ -17,44 +17,42 @@ namespace alg
         return GCD(bigger % less, less);
     }
 
-    std::uint32_t GCD(std::vector<std::uint32_t> const &values)
+    std::uint64_t GCD(std::vector<std::uint64_t> const &values)
     {
-        auto res = values.front();
-        for (std::int32_t i = 1; i < static_cast<std::int32_t>(values.size()); ++i)
-        {
+        auto res = GCD(values[0], values[1]);
+        for (std::int32_t i = 2; i < static_cast<std::int32_t>(values.size()); ++i)
             res = GCD(res, values[i]);
-        }
 
         return res;
     }
 
-    std::uint32_t LCM(std::vector<std::uint32_t> const& values)
+    std::uint64_t LCM(std::uint64_t a, std::uint64_t b)
     {
-        auto gcd = values.front();
-        auto tmp = gcd;
-        for (std::int32_t i = 1; i < static_cast<std::int32_t>(values.size()); ++i)
-        {
-            tmp = gcd;
-            gcd = GCD(gcd, values[i]);
-        }
+        return (a / GCD(a, b)) * b;
+    }
 
-        auto res = (tmp * values.back()) / gcd;
+    std::uint64_t LCM(std::vector<std::uint64_t> const& values)
+    {
+        auto res = LCM(values[0], values[1]);
+        for (std::int32_t i = 2; i < static_cast<std::int32_t>(values.size()); ++i)
+            res = LCM(res, values[i]);
+
         return res;
     }
 }
 
 int main()
 {
-    std::uint32_t n;
+    std::uint64_t n;
     std::cin >> n;
 
-    std::vector<std::uint32_t> v(n);
-    std::copy_n(std::istream_iterator<std::uint32_t>{std::cin}, v.size(), v.begin());
+    std::vector<std::uint64_t> v(n);
+    std::copy_n(std::istream_iterator<std::uint64_t>{std::cin}, v.size(), v.begin());
 
     // 3.2.2
-     auto res = alg::GCD(v);
-    // 3.2.33
-    //auto res = alg::LCM(v);
+    //  auto res = alg::GCD(v);
+    // 3.2.3
+    auto res = alg::LCM(v);
     std::cout << res << std::endl;
 
     return 0;
