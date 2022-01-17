3ddde376f65578c6e29fe176327471f2c3a08464
a_comma <noblesseoblige6@gmail.com>
2022-01-17 21:50:28 +0900

    solve the problem

--- MathAlgorithm/3.4/ExpectedValue.cpp
+++ MathAlgorithm/3.4/ExpectedValue.cpp
@@ -1,27 +1,23 @@
 #include <iostream>
 #include <algorithm>
 #include <iterator>
-#include <numeric>
 #include <cmath>
-#include <array>
 #include <vector>
 
 int main()
 {
-    std::uint64_t n;
+    std::uint32_t n;
     std::cin >> n;
 
-    std::vector<std::uint64_t> a(n);
-    std::copy_n(std::istream_iterator<std::uint64_t>{std::cin}, a.size(), a.begin());
+    std::vector<std::uint32_t> a(n);
+    std::copy_n(std::istream_iterator<std::uint32_t>{std::cin}, a.size(), a.begin());
 
-    std::vector<std::uint64_t> b(n);
-    std::copy_n(std::istream_iterator<std::uint64_t>{std::cin}, b.size(), b.begin());
+    std::vector<std::uint32_t> b(n);
+    std::copy_n(std::istream_iterator<std::uint32_t>{std::cin}, b.size(), b.begin());
 
-    decltype(n) res = 0;
+    std::double_t res = 0;
     for (decltype(n) i = 0; i < n; ++i)
-    {
-        res += (1.0f / 3.0f) * a[i] + (2.0f / 3.0f) * b[i];
-    }
+        res += (a[i]+ (2.0 * b[i])) / 3.0;
 
     std::cout << res << std::endl;
 
