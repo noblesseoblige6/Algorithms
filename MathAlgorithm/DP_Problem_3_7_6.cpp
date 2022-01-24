11f7fa2a0cb44e5f30599cfb21817841759970e3
a_comma <noblesseoblige6@gmail.com>
2022-01-24 21:17:21 +0900

    work on a problem

--- MathAlgorithm/DP_Problem_3_7_6.cpp
+++ MathAlgorithm/DP_Problem_3_7_6.cpp
@@ -9,17 +9,29 @@ int main()
     std::uint32_t n;
     std::cin >> n;
 
-    std::vector<std::int32_t> a(n+1, 0);
-    std::copy_n(std::istream_iterator<std::int32_t>{std::cin}, n, a.begin()+1);
+    std::vector<std::uint32_t> a(n+1, 0);
+    std::copy_n(std::istream_iterator<std::uint32_t>{std::cin}, n, a.begin()+1);
 
     std::vector<std::vector<std::uint64_t>> dp(n+1, std::vector<std::uint64_t>(n+1, 0));
-    for (auto i = 1; i <= n; ++i)
-        for (auto j = 0; j <= n; ++j)
+
+    for (decltype(n) i = 1; i <= n; ++i)
+        for (decltype(n) j = 0; j <= n; ++j)
         {
             if(j < 2)
-                dp[i][j] = (j < 1) ? dp[i - 1][j] : dp[i - 1][0] + a[i];
+            {
+                dp[i][j] = std::max(dp[i-1][j], static_cast<std::uint64_t>(a[j]));
+            }
             else
-                dp[i][j] = (j >= 2) ? dp[i - 1][j] : std::max(dp[i - 1][j], dp[i - 1][j-2] + a[i]);
+            {
+                if(j > i)
+                {
+                    dp[i][j] = dp[i][i];
+                }
+                else
+                {
+                    dp[i][j] = std::max(dp[i-1][j], dp[i-1][j-2] + a[i]);
+                }
+            }
         }
 
     std::cout << dp[n][n] << std::endl;
