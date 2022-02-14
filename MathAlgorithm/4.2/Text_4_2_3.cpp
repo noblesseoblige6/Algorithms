48d1be81f04a0ea7716a5580237b864b1c541182
a_comma <noblesseoblige6@gmail.com>
2022-02-15 08:34:08 +0900

    refactoring

--- MathAlgorithm/4.2/Text_4_2_3.cpp
+++ MathAlgorithm/4.2/Text_4_2_3.cpp
@@ -8,21 +8,16 @@ int main()
     std::vector<std::uint32_t> snows(q, 0);
     std::vector<std::pair<std::uint32_t, std::uint32_t>> districts(q);
     for(auto i = 0; i < q; ++i)
-    {
-        std::uint32_t l, r, x;
-        std::cin >> l >> r >> x;
-        districts[i] = {l, r};
-        snows[i] = x;
-    }
+        std::cin >> districts[i].first >> districts[i].second >> snows[i];
 
-    std::vector<std::int32_t> res(n+2, 0);
+    std::vector<std::int32_t> res(n+1, 0);
     for(auto i = 0; i < q; ++i)
     {
-        res[districts[i].first] += snows[i];
-        res[districts[i].second+1] -= snows[i];
+        res[districts[i].first-1] += snows[i];
+        res[districts[i].second] -= snows[i];
     }
 
-    for(auto i = 1; i < n; ++i)
+    for(auto i = 0; i < n-1; ++i)
     {
         if (res[i+1] > 0)
             std::cout << "<";
