f17fa98dd21585b10cd4d092334ac44cebd5ff62
a_comma <noblesseoblige6@gmail.com>
2022-02-18 20:20:38 +0900

    try problem 4.2.1

---
+++ MathAlgorithm/4.2/Problem_4_2_1.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+int main()
+{
+    std::uint32_t n, m;
+    std::cin >> n;
+    
+    std::vector<std::uint32_t> a(n, 0);
+    std::copy_n(std::istream_iterator<std::uint32_t>{std::cin}, n-1, a.begin()+1);
+
+    std::cin >> m;
+    std::vector<std::uint32_t> b(m, 0);
+    std::copy_n(std::istream_iterator<std::uint32_t>{std::cin}, m, b.begin());
+
+    std::vector<std::uint32_t> cusum(n, 0);
+    for(auto i = 1; i < n; ++i)
+        cusum[i] = a[i] + cusum[i - 1];
+
+    std::uint32_t res = 0;
+    for (auto i = 1; i < m; ++i)
+    {
+        auto i_big = std::max(b[i]-1, b[i - 1]-1);
+        auto i_less = std::min(b[i]-1, b[i - 1]-1);
+        res += cusum[i_big] - cusum[i_less];
+    }
+
+    std::cout << res << std::endl;
+}
