9332af7ab65750cc112347d3f4f8fcfb27aabd46
a_comma <noblesseoblige6@gmail.com>
2022-04-03 01:27:41 +0900

    WIP: solving

--- MathAlgorithm/5.4/Problem_5_4_5.cpp
+++ MathAlgorithm/5.4/Problem_5_4_5.cpp
@@ -1,28 +1,78 @@
 #include <bits/stdc++.h>
 
+namespace alg
+{
+    std::uint64_t GCD(std::uint64_t a, std::uint64_t b)
+    {
+        if (a == 0 || b == 0)
+            return std::max(a, b);
+
+        auto bigger = std::max(a, b);
+        auto less = std::min(a, b);
+
+        return GCD(bigger % less, less);
+    }
+
+    std::uint64_t GCD(std::vector<std::uint64_t> const &values)
+    {
+        auto res = GCD(values[0], values[1]);
+        for (std::int32_t i = 2; i < static_cast<std::int32_t>(values.size()); ++i)
+            res = GCD(res, values[i]);
+
+        return res;
+    }
+
+    std::uint64_t LCM(std::uint64_t a, std::uint64_t b)
+    {
+        //Note: divide first to avoid overflow
+        return (a / GCD(a, b)) * b;
+    }
+
+    std::uint64_t LCM(std::vector<std::uint64_t> const& values)
+    {
+        auto res = LCM(values[0], values[1]);
+        for (std::int32_t i = 2; i < static_cast<std::int32_t>(values.size()); ++i)
+            res = LCM(res, values[i]);
+
+        return res;
+    }
+
+    template <typename T>
+    bool is_pow2(T x)
+    {
+        if (x == 0)
+            return false;
+
+        return (x & (x - 1)) == 0;
+    }
+}
+
 int main()
 {
     std::uint64_t n, k;
     std::cin >> n >> k;
 
     std::vector<std::uint32_t> v(k);
-    std::copy_n(std::istream_iterator<std::uint64_t>{std::cin}, v.size(), v.begin());
-
-    std::vector<bool> isMultiple(n+1, false);
-    for (decltype(n) i = 1; i <= n; ++i)
-    {
-        for(auto val : v)
-        {
-            if (i % val != 0)
-                continue;
-
-            isMultiple[i] = true;
-        }
-    }
+    std::copy_n(std::istream_iterator<std::uint64_t>{std::cin}, k, v.begin());
 
     std::uint32_t res = 0;
-    for (auto isMul : isMultiple)
-        res += (isMul ? 1 : 0);
+    for(std::uint32_t i = 1; i < (1 << k); ++i)
+    {
+        std::uint32_t lcm = 0;
+        for(std::uint32_t j = 0; j < k; ++j)
+        {
+            if ((i & (1 << j)) == 0)
+                continue;
+
+            lcm = (lcm == 0) ? v[j] : alg::LCM(lcm, v[j]);
+        }
+
+        std::uint32_t multi =  static_cast<std::uint32_t>(n / lcm);;
+        if (alg::is_pow2(i))
+            res += multi;
+        else
+            res -= multi;
+    }
 
     std::cout << res << std::endl;
 
