0c7d55d3397659a105b39a581e358900dfd0040b
a_comma <noblesseoblige6@gmail.com>
2022-04-12 06:11:06 +0900

    solve text 4.7.6

--- MathAlgorithm/4.7/Text_4_7_6.cpp
+++ MathAlgorithm/4.7/Text_4_7_6.cpp
@@ -4,7 +4,7 @@ namespace alg
 {
     struct Matrix
     {
-        std::int32_t p[2][2] = {{1, 0}, {0, 1}};
+        std::uint64_t p[2][2] = {{1, 0}, {0, 1}};
     };
 
     Matrix Multiply(Matrix const &a, Matrix const &b)
@@ -17,7 +17,7 @@ namespace alg
                 for (auto k = 0; k < 2; ++k)
                 {
                     c.p[i][k] += a.p[i][j] * b.p[j][k];
-                    c.p[i][k] %= static_cast<std::int32_t>(1e9);
+                    c.p[i][k] %= 1000000000;
                 }
             }
         }
@@ -29,12 +29,11 @@ namespace alg
     {
         Matrix tmp = a;
         Matrix res = ((n & 1) != 0) ? tmp : Matrix();
-        for(decltype(n) i = 1; i < 64; ++i)
+        for(auto i = 1; i < 64; ++i)
         {
+            tmp = Multiply(tmp, tmp);
             if ((n & (1ULL << i)) != 0ULL)
                 res = Multiply(res, tmp);
-
-            tmp = Multiply(tmp, tmp);
         }
 
         return res;
@@ -47,8 +46,8 @@ int main()
     std::cin >> n;
 
     alg::Matrix a = {1, 1, 1, 0};
-    auto mat = alg::Power(a, n-1);
-    std::cout << (mat.p[1][0] + mat.p[1][1]) % static_cast<std::int32_t>(1e9) << std::endl;
+    auto res = alg::Power(a, n-1);
+    std::cout << (res.p[1][0] + res.p[1][1]) % 1000000000 << std::endl;
 
     return 0;
 }
