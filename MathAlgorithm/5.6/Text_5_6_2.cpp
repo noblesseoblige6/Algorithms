b57ec8506662c60c58443ac66cf39b84a4005f96
a_comma <noblesseoblige6@gmail.com>
2022-04-16 14:03:35 +0900

    study 5.6

---
+++ MathAlgorithm/5.6/Text_5_6_2.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+int main()
+{
+    std::uint32_t a, b;
+    std::cin >> a >> b;
+
+    std::uint32_t res = 1;
+    for (decltype(a) i = 1; i <= b; ++i)
+    {
+        if (std::floor(static_cast<float>(b) / i) - std::ceil(static_cast<float>(a) / i) >= 1)
+        {
+            res = i;
+        }
+    }
+
+    std::cout << res << std::endl;
+
+    return 0;
+}
